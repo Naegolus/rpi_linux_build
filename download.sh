47b2155dd9cb8d4c08b6f6ef8fa5fc6b6d297e14
Johannes Natter <redrocket@gmx.at>
2015-12-30 20:18:38 +0100

    R-Pi Base: Create directory overlays in boot on target

--- download.sh
+++ download.sh
@@ -15,7 +15,7 @@ fi
 
 sect=$(echo ${sections} | sed "s:,: :g")
 kvers=$(cat src/linux/include/generated/utsrelease.h | cut --delimiter="\"" --fields=2)
-files=""
+files=
 
 echo "Downloading:"
 for s in ${sect}
@@ -23,32 +23,30 @@ do
 	if [ "$s" == "boot" ]; then
 		echo "- boot files"
 		bootfiles=$(ls -1 output/boot/ | grep --invert-match --extended-regexp "overlays|bcm2708|config|linux.*img" | sed "s:^:boot/:")
-		files="${files} ${bootfiles}"
+		files="${files}"$'\n'"${bootfiles}"
 	fi
 
 	if [ "$s" == "dts" ]; then
 		echo "- device tree"
 		maindtb=$(ls -1 output/boot/*.dtb | sed "s:output/::")
 		overlays=$(ls -1 output/boot/overlays/*.dtb | sed "s:output/::")
-		files="${files} ${maindtb} ${overlays}"
+		files="${files}"$'\n'"${maindtb}"$'\n'"${overlays}"
 	fi
 
 	if [ "$s" == "kernel" ]; then
 		echo "- config file"
 		echo "- kernel image"
-		files="${files} boot/config.txt boot/linux-${kvers}.img"
+		files="${files}"$'\n'"boot/config.txt"$'\n'"boot/linux-${kvers}.img"
 	fi
 
 	if [ "$s" == "modules" ]; then
 		echo "- modules"
-		files="${files} modules-${kvers}.tar.gz"
+		files="${files}"$'\n'"modules-${kvers}.tar.gz"
 	fi
 done
 
-echo "Files: ${files}"
-
-exit 1
-
 if [ ! "${files}" == "" ]; then
-	scp ${files} root@$1:/boot/.
+	cd output
+	ssh root@$1 'mkdir -p /boot/overlays'
+	#scp ${files} root@$1:/.
 fi
