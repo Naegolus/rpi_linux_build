1312cc54657cfa68c0462081cff7831720d7656f
Johannes Natter <redrocket@gmx.at>
2015-12-30 20:36:15 +0100

    R-Pi Base: Finalize download.sh

--- download.sh
+++ download.sh
@@ -15,38 +15,30 @@ fi
 
 sect=$(echo ${sections} | sed "s:,: :g")
 kvers=$(cat src/linux/include/generated/utsrelease.h | cut --delimiter="\"" --fields=2)
-files=
 
-echo "Downloading:"
 for s in ${sect}
 do
 	if [ "$s" == "boot" ]; then
-		echo "- boot files"
-		bootfiles=$(ls -1 output/boot/ | grep --invert-match --extended-regexp "overlays|bcm2708|config|linux.*img" | sed "s:^:boot/:")
-		files="${files}"$'\n'"${bootfiles}"
+		echo "### Downloading boot files"
+		bootfiles=$(ls -1 output/boot/ | grep --invert-match --extended-regexp "overlays|bcm2708|config|linux.*img" | sed "s:^:output/boot/:")
+		scp ${bootfiles} root@$1:/boot/.
 	fi
 
 	if [ "$s" == "dts" ]; then
-		echo "- device tree"
-		maindtb=$(ls -1 output/boot/*.dtb | sed "s:output/::")
-		overlays=$(ls -1 output/boot/overlays/*.dtb | sed "s:output/::")
-		files="${files}"$'\n'"${maindtb}"$'\n'"${overlays}"
+		echo "### Downloading device tree"
+		ssh root@$1 'mkdir -p /boot/overlays'
+		scp output/boot/bcm2708-rpi-b.dtb root@$1:/boot/.
+		overlays=$(ls -1 output/boot/overlays/*.dtb)
+		scp ${overlays} root@$1:/boot/overlays/.
 	fi
 
 	if [ "$s" == "kernel" ]; then
-		echo "- config file"
-		echo "- kernel image"
-		files="${files}"$'\n'"boot/config.txt"$'\n'"boot/linux-${kvers}.img"
+		echo "### Downloading kernel image and config file"
+		scp output/boot/config.txt output/boot/linux-${kvers}.img root@$1:/boot/.
 	fi
 
 	if [ "$s" == "modules" ]; then
-		echo "- modules"
-		files="${files}"$'\n'"modules-${kvers}.tar.gz"
+		echo "### Downloading modules"
+		scp output/modules-${kvers}.tar.gz root@$1:/tmp/.
 	fi
 done
-
-if [ ! "${files}" == "" ]; then
-	cd output
-	ssh root@$1 'mkdir -p /boot/overlays'
-	#scp ${files} root@$1:/.
-fi
