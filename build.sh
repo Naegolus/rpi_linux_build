0dfc4b2144bb1d6e1f55f6a80e616f85cbd0f71b
Johannes Natter <redrocket@gmx.at>
2015-12-30 02:42:02 +0100

    R-Pi Base: Add download section in build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,14 @@
 wd=$(pwd)
 
 mkdir -p src output
+
+# Download repositories
+if [ ! -d src/linux ]; then
+	echo "Downloading Raspberry Pi linux"
+	git clone git@github.com:Naegolus/linux.git src/linux
+fi
+
+if [ ! -d src/tools ]; then
+	echo "Downloading Raspberry Pi tools"
+	git clone git@github.com:Naegolus/tools src/tools
+fi
