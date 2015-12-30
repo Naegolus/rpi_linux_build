1de01bd496a6394ea7905b441b94ed2cf0581612
Johannes Natter <redrocket@gmx.at>
2015-12-30 03:50:59 +0100

    R-Pi Base: Add section to download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
 if [ "$1" == "" ]; then
-	echo "Usage: $0 <ip>"
+	echo "Usage: $0 <ip> [section]"
+	echo ""
+	echo "sections: boot,dts,kernel,modules"
 	exit 1
+fi
+
+if [ "$2" == "boot" ]; then
+	echo "Downloading boot files"
+fi
+
+if [ "$2" == "dts" ]; then
+	echo "Downloading dts"
+fi
+
+if [ "$2" == "kernel" ]; then
+	echo "Downloading kernel"
+fi
+
+if [ "$2" == "modules" ]; then
+	echo "Downloading modules"
+fi
+
+if [ "$2" == "" ]; then
+	echo "Downloading all"
 fi
