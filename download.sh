8c480cd49c2106503bded3cebe0ac7801d672783
Johannes Natter <redrocket@gmx.at>
2015-12-30 19:46:36 +0100

    R-Pi Base: Implement download.h

--- download.sh
+++ download.sh
@@ -7,22 +7,48 @@ if [ "$1" == "" ]; then
 	exit 1
 fi
 
-if [ "$2" == "boot" ]; then
-	echo "Downloading boot files"
-fi
-
-if [ "$2" == "dts" ]; then
-	echo "Downloading dts"
-fi
-
-if [ "$2" == "kernel" ]; then
-	echo "Downloading kernel"
-fi
-
-if [ "$2" == "modules" ]; then
-	echo "Downloading modules"
-fi
-
 if [ "$2" == "" ]; then
-	echo "Downloading all"
+	sections="boot,dts,kernel,modules"
+else
+	sections="$2"
+fi
+
+sect=$(echo ${sections} | sed "s:,: :g")
+kvers=$(cat src/linux/include/generated/utsrelease.h | cut --delimiter="\"" --fields=2)
+files=""
+
+echo "Downloading:"
+for s in ${sect}
+do
+	if [ "$s" == "boot" ]; then
+		echo "- boot files"
+		bootfiles=$(ls -1 output/boot/ | grep --invert-match --extended-regexp "overlays|bcm2708|config|linux.*img" | sed "s:^:boot/:")
+		files="${files} ${bootfiles}"
+	fi
+
+	if [ "$s" == "dts" ]; then
+		echo "- device tree"
+		maindtb=$(ls -1 output/boot/*.dtb | sed "s:output/::")
+		overlays=$(ls -1 output/boot/overlays/*.dtb | sed "s:output/::")
+		files="${files} ${maindtb} ${overlays}"
+	fi
+
+	if [ "$s" == "kernel" ]; then
+		echo "- config file"
+		echo "- kernel image"
+		files="${files} boot/config.txt boot/linux-${kvers}.img"
+	fi
+
+	if [ "$s" == "modules" ]; then
+		echo "- modules"
+		files="${files} modules-${kvers}.tar.gz"
+	fi
+done
+
+echo "Files: ${files}"
+
+exit 1
+
+if [ ! "${files}" == "" ]; then
+	scp ${files} root@$1:/boot/.
 fi
