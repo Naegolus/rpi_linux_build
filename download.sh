24f96643ce6ecf3dfa38f2b8c70778c57897ee89
Johannes Natter <office@dsp-crowd.com>
2017-07-22 14:11:54 +0200

    download.sh: operator bugfix

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 allsec="firmware,config,dts,kernel,modules"
 defsec="${allsec}"
 
-if [ "$1" == "" ]; then
+if [ "$1" = "" ]; then
 	echo "Usage: $0 <ip> [section]"
 	echo ""
 	echo "sections"
@@ -12,7 +12,7 @@ if [ "$1" == "" ]; then
 	exit 1
 fi
 
-if [ "$2" == "" ]; then
+if [ "$2" = "" ]; then
 	sections="${defsec}"
 else
 	sections="$2"
@@ -23,18 +23,18 @@ kvers=$(cat src/linux/include/generated/utsrelease.h | cut --delimiter="\"" --fi
 
 for s in ${sect}
 do
-	if [ "$s" == "firmware" ]; then
+	if [ "$s" = "firmware" ]; then
 		echo "### Downloading firmware files"
 		bootfiles=$(ls -1 output/boot/ | grep --invert-match --extended-regexp "overlays|bcm2708|config|linux.*img" | sed "s:^:output/boot/:")
 		scp ${bootfiles} root@$1:/boot/.
 	fi
 
-	if [ "$s" == "config" ]; then
+	if [ "$s" = "config" ]; then
 		echo "### Downloading kernel image and config file"
 		scp output/boot/config.txt root@$1:/boot/.
 	fi
 
-	if [ "$s" == "dts" ]; then
+	if [ "$s" = "dts" ]; then
 		echo "### Downloading device tree"
 		ssh root@$1 'mkdir -p /boot/overlays'
 		scp output/boot/bcm2708-rpi-b.dtb root@$1:/boot/.
@@ -42,12 +42,12 @@ do
 		scp ${overlays} root@$1:/boot/overlays/.
 	fi
 
-	if [ "$s" == "kernel" ]; then
+	if [ "$s" = "kernel" ]; then
 		echo "### Downloading kernel image and config file"
 		scp output/boot/linux-${kvers}.img root@$1:/boot/.
 	fi
 
-	if [ "$s" == "modules" ]; then
+	if [ "$s" = "modules" ]; then
 		echo "### Downloading modules"
 		scp output/modules-${kvers}.tar.gz root@$1:/tmp/.
 	fi
